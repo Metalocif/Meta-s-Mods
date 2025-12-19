f20f8d26fe8be7291fbc045f56e11a3da27db0f4
Metalocif <62672020+Metalocif@users.noreply.github.com>
2025-12-19 18:12:33 +0100

    More CEO dialog.

--- mods/RogueMechs/scripts/missions/mission_roguemechs_dialog.lua
+++ mods/RogueMechs/scripts/missions/mission_roguemechs_dialog.lua
@@ -3,49 +3,62 @@ return {
 	CEO_Snow = {
 		Briefing = {
 			"You are not the first time travelers to try and save us. The others failed; it seems their mechs were hijacked by the Vek. Please, put them down.",
+			"Those machines still carry pilot imprints. Ending them is an act of mercy, not destruction.",
 		},
 		Failure = {
 			"Will these machines never find rest?",
+			"I will archive their combat patterns. It is all I can do now.",
 			"My bots do not have the firepower or armor to fight on an equal footing with mechs. If you cannot deal with them, this island must be lost.",
 		},
 		Success = {
-			"Great work, commander."
+			"Great work, commander.",
+			"Their frames are destroyed, but their data will endure. It will be used to raise leader bots.", 
 		},
 	},
 	
 	CEO_Sand = {
 		Briefing = {
 			"Others much like your squad visited this island before you arrived. They fell in battle against the Vek to save my people; worse, the Vek have infected and puppeteer their mechs. Put them down.",
+			"Those mechs once had good pilots. The Vek didn’t win them clean.",
 		},
 		Failure = {
 			"I am surprised the Vek are better at piloting mechs than your pilots.",
+			"I’ve seen better squads fall. I hoped you weren’t one of them.",
 		},
 		Success = {
-			"Great work. I'd rather keep all such war machines on our side."
+			"Great work. I'd rather keep all such war machines on our side.",
+			"This desecration is over. Thank you.",
 		},
 	},
 	
 	CEO_Grass = {
 		Briefing = {
 			"You are not the first time travelers to try and save us. The others failed; it seems their mechs were hijacked by the Vek. Please, put them down.",
+			"They fought to save this island once. I won’t let them be remembered as monsters.",
+			"Back home, machines were built to protect people, not be their graves."
 		},
 		Failure = {
 			"Will the metallic tombs of these brave pilots never find rest?",
+			"Another battlefield we won’t sing about.",
 		},
 		Success = {
-			"This battle was fit for the threaters of Old Earth!"
+			"This battle was fit for the theaters of Old Earth!",
+			"This island once was a museum. Let it be a graveyard of heroes, after the war."
 		},
 	},
 	
 	CEO_Acid = {
 		Briefing = {
 			"You are not the first time travelers to try and save us. The others failed; it seems their mechs were hijacked by the Vek. Please, put them down.",
+			"Vek took over some mechs. Try not to scatter the wreckage too much. Cleanup’s already a nightmare.",
 		},
 		Failure = {
 			"We'll have to drown these mechs in A.C.I.D. to purge the infection, further draining our limited resources.",
+			"Scrap is already enough trouble when it's not trying to kill us."
 		},
 		Success = {
-			"Great work, commander."
+			"Great work, commander.",
+			"Good. We can process what’s left safely now."
 		},
 	},
 }
